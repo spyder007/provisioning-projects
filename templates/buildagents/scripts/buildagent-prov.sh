a5cb7784238e9b10ad1d1fe29e047099109662ed
Matt Gerega <gerega@gmail.com>
2021-03-29 18:06:24 -0400

    moved image temp folder

---
+++ templates/buildagents/scripts/buildagent-prov.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+  
+sudo lvextend -l+100%FREE $(sudo lvdisplay -C -o "lv_path" --noheadings)
+sudo resize2fs $(df / --output=source | sed -e /Filesystem/d)
+sudo apt update
+sudo apt upgrade -y
+mkdir ~/.ssh
+cp ~/packertmp/authorized_keys ~/.ssh/
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
