4d30ab98332c4098984f4978362d9decbf49c03d
Matt Gerega <gerega@gmail.com>
2021-03-30 15:50:32 -0400

    Removed extraneous file copy and validation steps

--- templates/buildagents/scripts/buildagent-prov.sh
+++ templates/buildagents/scripts/buildagent-prov.sh
@@ -5,5 +5,5 @@ sudo resize2fs $(df / --output=source | sed -e /Filesystem/d)
 sudo apt update
 sudo apt upgrade -y
 mkdir ~/.ssh
-cp ~/packertmp/authorized_keys ~/.ssh/
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+cp ~/imagegeneration/authorized_keys ~/.ssh/
+
