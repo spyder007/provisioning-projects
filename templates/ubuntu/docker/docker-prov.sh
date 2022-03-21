955d0186631d24a9dd060b529852fae4708a82ef
Matt Gerega <gerega@gmail.com>
2022-03-21 10:51:22 -0400

    Updated MS Build agent version and fixed docker provisioning

--- templates/ubuntu/docker/docker-prov.sh
+++ templates/ubuntu/docker/docker-prov.sh
@@ -4,4 +4,5 @@ sudo apt install docker.io -y
 sudo cp ~/packertmp/daemon.json /etc/docker/
 sudo systemctl enable docker
 sudo systemctl start docker
-      
+sudo usermod -aG docker $USER
+sudo swapoff -a
