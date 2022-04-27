4bbd4dff689a869def27b988f04c09aa70ace858
Matt Gerega <gerega@gmail.com>
2022-04-27 11:34:22 -0400

    Added Script to provision multiple clusters - generic naming

--- Create-Cluster.ps1
+++ Create-Cluster.ps1
@@ -1,22 +1,16 @@
 param (
     [Parameter(Mandatory=$true,Position=1)]
 	$baseName,
-    $additionalNodes=2
+    $nodeCount=3
 )
 
 $packerTemplate = ".\templates\ubuntu\ubuntu-2204.pkr.hcl"
 $httpFolder = ".\templates\ubuntu\basic\http\"
 $packerVariables = ".\templates\ubuntu\docker\k8-nonprod-main.pkrvars.hcl"
 
-## Create Main
-
-$machineName = "$baseName-main"
-Write-Host "Building $machineName"
-Invoke-Expression ".\Build-Ubuntu.ps1 $packerTemplate $httpFolder $packerVariables -machineName $machineName"
-
-## Create Additional Nodes
-for ($i=1; $i -le $additionalNodes; $i++) {
-    $machineName = "$baseName-$i"
+## Create Nodes
+for ($i=1; $i -le $nodeCount; $i++) {
+    $machineName = "$baseName-n$i"
     Write-Host "Building $machineName"
     Invoke-Expression ".\Build-Ubuntu.ps1 $packerTemplate $httpFolder $packerVariables -machineName $machineName"
 }
