b7d228c1218e8711e162b80e7b17b3be5214e03b
Matt Gerega <gerega@gmail.com>
2022-07-14 09:57:45 -0400

    Modified Create-NewBuildAgent with type parameter

--- Create-NewBuildAgent.ps1
+++ Create-NewBuildAgent.ps1
@@ -1,5 +1,8 @@
 param (
     [Parameter()]
+    [ValidateSet("ubuntu-22.04", "ubuntu-20.04")]
+    $type,
+    [Parameter()]
     $OutputFolder="d:\\Virtual Machines\\"
 )
 
@@ -7,7 +10,5 @@ param (
 $agentDate=(Get-Date).ToString("yyMMdd")
 $machineName = "agt-ubt-$agentDate"
 
-
-
 ## Create and Provision agent
-.\Build-Ubuntu.ps1 ".\templates\buildagents\ubuntu-2004.pkr.hcl" .\templates\buildagents\http\ .\templates\buildagents\buildagent.pkrvars.hcl -OutputFolder "$OutputFolder" -machineName $machineName
+.\Build-Ubuntu.ps1 ".\templates\buildagents\$($type).pkr.hcl" .\templates\buildagents\http\ .\templates\buildagents\buildagent.pkrvars.hcl -OutputFolder "$OutputFolder" -machineName $machineName
