e04b0fb3e5543d24ce9c319e18377d4f879a508c
Matt Gerega <gerega@gmail.com>
2023-01-17 14:24:37 -0500

    Consolidated provisioning into a module

--- Create-NewBuildAgent.ps1
+++ Create-NewBuildAgent.ps1
@@ -8,9 +8,11 @@ param (
     $packerErrorAction = "cleanup"
 )
 
+Import-Module ./HyperV-Provisioning.psm1
+
 ## Generate agent name
 $agentDate=(Get-Date).ToString("yyMMdd")
 $machineName = "agt-ubt-$agentDate"
 
 ## Create and Provision agent
-.\Build-Ubuntu.ps1 ".\templates\buildagents\$($type).pkr.hcl" .\templates\buildagents\http\ .\templates\buildagents\buildagent.pkrvars.hcl -packerErrorAction "$packerErrorAction" -OutputFolder "$OutputFolder" -machineName $machineName
+Build-Ubuntu -TemplateFile ".\templates\buildagents\$($type).pkr.hcl" -HostHttpFolder ".\templates\buildagents\http\" -VariableFile ".\templates\buildagents\buildagent.pkrvars.hcl" -packerErrorAction "$packerErrorAction" -OutputFolder "$OutputFolder" -machineName $machineName
