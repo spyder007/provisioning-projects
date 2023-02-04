cf3e9f85f48af1fd6084d4b5201fdebd8adc776c
Matt Gerega <gerega@gmail.com>
2023-02-04 12:02:39 -0500

    Fixed install server script (added sudo)

--- templates/ubuntu/rke2/scripts/install-server.sh
+++ templates/ubuntu/rke2/scripts/install-server.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-curl -sfL https://get.rke2.io | sh -
-systemctl enable rke2-server.service
-systemctl start rke2-server.service
+curl -sfL https://get.rke2.io --output install.sh
+sudo chmod +x install.sh
+
+sudo ./install.sh
+sudo systemctl enable rke2-server.service
+sudo systemctl start rke2-server.service
