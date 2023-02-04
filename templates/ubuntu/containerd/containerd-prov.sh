8a0fea683979e2e3b7499a8af88d5f242adbfdee
Matt Gerega <gerega@gmail.com>
2023-02-04 11:31:32 -0500

    New provisioning for RKE2

--- templates/ubuntu/containerd/containerd-prov.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-sudo apt install containerd.io -y
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-sudo systemctl restart containerd
-      
