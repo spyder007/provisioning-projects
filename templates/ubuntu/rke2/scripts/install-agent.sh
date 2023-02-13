7123f709d0a1ae1b88a5c060a7e3ceb1162fc836
Matt Gerega <gerega@gmail.com>
2023-02-13 09:02:44 -0500

    Use built-in ingress for RKE2 clusters

--- templates/ubuntu/rke2/scripts/install-agent.sh
+++ templates/ubuntu/rke2/scripts/install-agent.sh
@@ -7,5 +7,6 @@ sudo INSTALL_RKE2_TYPE="agent" ./install.sh
 sudo systemctl enable rke2-agent.service
 sudo mkdir -p /etc/rancher/rke2
 sudo cp ~/packertmp/agent-config.yaml /etc/rancher/rke2/config.yaml
+sudo cp ~/packtertmp/manifests/* /var/lib/rancher/rke2/server/manifests/
 
 sudo systemctl start rke2-agent.service
