7df96e9e5c70f011d68aff81565cffb976de6dd8
Matt Gerega <gerega@gmail.com>
2023-02-16 09:53:48 -0500

    Testing quick provisioning

--- HyperV-Provisioning.psm1
+++ HyperV-Provisioning.psm1
@@ -1,3 +1,111 @@
+function Build-UbuntuBase {
+      <#
+        .SYNOPSIS
+        Create an Ubuntu hyper-v image using Packer
+
+        .DESCRIPTION
+        Create an Ubuntu hyper-v image using Packer.  This script does some specific work to translate passwords
+        that are specific to Ubuntu.
+
+        .PARAMETER TemplateFile
+        The location of the Packer template file (*.json or *.hcl)
+
+        .PARAMETER HostHttpFolder
+        The locatikon of the HostHTTP Folder.  This folder is mounted for the network installer to read.
+
+        .PARAMETER VariableFile
+        The location of the .pkvars file for this run
+
+        .PARAMETER OutputFolder
+        The base folder where the VM information will be stored.
+
+        .PARAMETER packerErrorAction
+        The ErrorAction to use for the Packer Command.  Valid values are "cleanup", "abort", "ask", and "run-cleanup-provisioner".
+        See https://developer.hashicorp.com/packer/docs/commands/build for information on the -on-error option details.
+
+        .PARAMETER machineName
+        The machine name to use for the final VM
+
+        .EXAMPLE
+        PS> .\Build-Ubuntu.ps1 ".\templates\ubuntu\ubuntu-2004.json" .\templates\ubuntu\basic\http .\templates\ubuntu\basic\basic.pkvars -machinename ubuntuHost
+    #>
+
+    param (
+        [Parameter(Position = 1)]
+        $TemplateFile = ".\templates\ubuntu-base\ubuntu-2204-base.pkr.hcl",
+        [Parameter(Position = 2)]
+        $HostHttpFolder = ".\templates\ubuntu-base\http",
+        [Parameter(Position = 3)]
+        $VariableFile = ".\templates\ubuntu-base\base.pkrvars.hcl",
+        [Parameter(Position = 4)]
+        [ValidateSet("cleanup", "abort", "ask", "run-cleanup-provisioner")]
+        $packerErrorAction = "cleanup",
+        [Parameter(Position = 5)]
+        $OutputFolder = "d:\\Virtual Machines\\",
+        [Parameter(Position = 6)]
+        [String]
+        $machineName = "ubuntu-2204-base"
+    )
+
+    if ($useUnifi) {
+        Import-Module ./Unifi.psm1
+    }
+
+    $vars = @{}
+    ## Grab the variables file
+    if (($null -ne $VariableFile) -and (Test-Path $VariableFile)) {
+        $variableLines = Get-Content $VariableFile
+    
+        foreach ($varLine in $variableLines) {
+            if ($varLine -match "(?<var>[^=]*)=(?<value>.*)") {
+                $vars[$matches.var.Trim().ToLower()] = $matches.value.Trim().Trim("`"")
+            }
+        }
+    }
+    else {
+        Write-Error "Variable file is required";
+        return -1;
+    }
+    if ($null -eq $machineName) {
+        $machineName = $vars["vm_name"]
+    }
+
+    ## crypt the password (unix style) so that it can go into the autoinstall folder
+    $cryptedPass = (Write-Output "$($vars["password"])" | openssl passwd -6 -salt "FFFDFSDFSDF" -stdin)
+
+    if (Test-Path "packerhttp") {
+        Remove-Item -Force -Recurse "packerhttp"
+    }
+
+    # Copy the contents
+    mkdir "packerhttp" | Out-Null
+    Copy-Item -Recurse "$HostHttpFolder\*" "packerhttp"
+
+    $user_data_content = Get-Content "packerhttp\user-data"
+    $user_data_content = $user_data_content -replace "{{username}}", "$($vars["username"])"
+    $user_data_content = $user_data_content -replace "{{crypted_password}}", "$cryptedPass"
+    $user_data_content = $user_data_content -replace "{{hostname}}", "$($machineName)"
+    $user_data_content | Set-Content "packerhttp\user-data"
+
+    $global:LASTEXITCODE = 0
+    $onError = "-on-error=$packerErrorAction"
+
+    Invoke-Expression "packer build $onError -var-file `"$VariableFile`" -var `"http=packerhttp`" -var `"output_dir=$OutputFolder`" -var `"vm_name=$machineName`" `"$TemplateFile`"" | Out-Host
+
+    $success = ($global:LASTEXITCODE -eq 0);
+
+    if ($success) {
+        $vmFolder = [IO.Path]::Combine($OutputFolder, $machineName)
+
+        return @{
+            success          = $true
+            machineName      = "$machineName"
+            baseLocation     = "$vmFolder"
+        }
+    }
+}
+
+
 function Build-Ubuntu {
     <#
         .SYNOPSIS
---
+++ templates/ubuntu-quick/rke2/scripts/install-server.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+curl -sfL https://get.rke2.io --output install.sh
+sudo chmod +x install.sh
+
+sudo ./install.sh
+
+# Copy the server configuration file
+sudo mkdir -p /etc/rancher/rke2
+sudo cp ~/packertmp/server-config.yaml /etc/rancher/rke2/config.yaml
+sudo cp ~/packtertmp/manifests/* /var/lib/rancher/rke2/server/manifests/
+
+sudo systemctl enable rke2-server.service
+sudo systemctl start rke2-server.service
+
+# Copy the creds file and node-token to an accessible location for future scripts
+sudo cp /etc/rancher/rke2/rke2.yaml .
+sudo chown $USER:$USER rke2.yaml
+sudo cp /var/lib/rancher/rke2/server/node-token .
+sudo chown $USER:$USER node-token
