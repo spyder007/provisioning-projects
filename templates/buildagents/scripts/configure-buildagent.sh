02ced918eb775c94eb5f16c5757fa47a9c016d86
Matt Gerega <gerega@gmail.com>
2024-01-30 16:53:07 -0500

    Updated agent provisioning to handle environment variables

--- templates/buildagents/scripts/configure-buildagent.sh
+++ templates/buildagents/scripts/configure-buildagent.sh
@@ -9,6 +9,7 @@ export AZP_AGENT_USE_LEGACY_HTTP=true
 sudo wget "$MS_AGENT_URL/$MS_AGENT_FILENAME"
 sudo tar zxvf ./$MS_AGENT_FILENAME
 sudo chmod -R 777 /myagent
+sudo cp /etc/agent_environment /myagent/.env
 ./config.sh --unattended  --url "$MS_AGENT_ORG_URL" --auth pat --token "$MS_AGENT_PAT" --pool "$MS_AGENT_POOL_NAME"
 sudo ./svc.sh install
 sudo ./svc.sh start
