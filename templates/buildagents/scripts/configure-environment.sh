63f28f2cc137f88a33c149cbf0f7d2751bea7f3a
Matt Gerega <gerega@gmail.com>
2024-01-30 16:53:07 -0500

    Updated agent provisioning to handle environment variables

--- templates/buildagents/scripts/configure-environment.sh
+++ templates/buildagents/scripts/configure-environment.sh
@@ -2,17 +2,18 @@
 
 # Source the helpers for use with the script
 source $HELPER_SCRIPTS/os.sh
+source $HELPER_SCRIPTS/etc-environment.sh
 
 # Set ImageVersion and ImageOS env variables
-echo ImageVersion=$IMAGE_VERSION | tee -a /etc/environment
-echo ImageOS=$IMAGE_OS | tee -a /etc/environment
+set_etc_environment_variable "ImageVersion" "${IMAGE_VERSION}"
+set_etc_environment_variable "ImageOS" "${IMAGE_OS}"
 
 # Set the ACCEPT_EULA variable to Y value to confirm your acceptance of the End-User Licensing Agreement
-#echo ACCEPT_EULA=Y | tee -a /etc/environment
+set_etc_environment_variable "ACCEPT_EULA" "Y"
 
-# This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/virtual-environments/issues/491)
-#mkdir -p /etc/skel/.config/configstore
-#echo 'XDG_CONFIG_HOME=$HOME/.config' | tee -a /etc/environment
+# This directory is supposed to be created in $HOME and owned by user(https://github.com/actions/runner-images/issues/491)
+mkdir -p /etc/skel/.config/configstore
+set_etc_environment_variable "XDG_CONFIG_HOME" '$HOME/.config'
 
 # Change waagent entries to use /mnt for swapfile
 #sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/g' /etc/waagent.conf
@@ -25,7 +26,7 @@ echo ImageOS=$IMAGE_OS | tee -a /etc/environment
 # Prepare directory and env variable for toolcache
 AGENT_TOOLSDIRECTORY=/opt/hostedtoolcache
 mkdir $AGENT_TOOLSDIRECTORY
-echo "AGENT_TOOLSDIRECTORY=$AGENT_TOOLSDIRECTORY" | tee -a /etc/environment
+set_etc_environment_variable "AGENT_TOOLSDIRECTORY" "${AGENT_TOOLSDIRECTORY}"
 chmod -R 777 $AGENT_TOOLSDIRECTORY
 
 # https://www.elastic.co/guide/en/elasticsearch/reference/current/vm-max-map-count.html
@@ -38,8 +39,8 @@ echo 'fs.inotify.max_user_instances=1280' | tee -a /etc/sysctl.conf
 
 # https://github.com/actions/runner-images/pull/7860
 netfilter_rule='/etc/udev/rules.d/50-netfilter.rules'
-rulesd="$(dirname "${netfilter_rule}")"
-mkdir -p $rulesd
+rules_directory="$(dirname "${netfilter_rule}")"
+mkdir -p $rules_directory
 touch $netfilter_rule
 echo 'ACTION=="add", SUBSYSTEM=="module", KERNEL=="nf_conntrack", RUN+="/usr/sbin/sysctl net.netfilter.nf_conntrack_tcp_be_liberal=1"' | tee -a $netfilter_rule
 
@@ -57,6 +58,32 @@ fi
 
 # Disable to load providers
 # https://github.com/microsoft/azure-pipelines-agent/issues/3834
-if isUbuntu22; then
+if is_ubuntu22; then
     sed -i 's/openssl_conf = openssl_init/#openssl_conf = openssl_init/g' /etc/ssl/openssl.cnf
 fi
+
+env | grep Image >> /etc/agent_environment
+env | grep ANDROID_ >> /etc/agent_environment
+env | grep JAVA_ >> /etc/agent_environment
+env | grep HOMEBREW_ >> /etc/agent_environment
+env | grep ANT_ >> /etc/agent_environment
+env | grep GRADLE_ >> /etc/agent_environment
+env | grep LEIN_ >> /etc/agent_environment
+env | grep CONDA >> /etc/agent_environment
+env | grep PIPX_ >> /etc/agent_environment
+env | grep AGENT_ >> /etc/agent_environment
+env | grep EDGEWEBDRIVER >> /etc/agent_environment
+env | grep CHROMEWEBDRIVER >> /etc/agent_environment
+env | grep CHROME_BIN >> /etc/agent_environment
+env | grep BOOTSTRAP_ >> /etc/agent_environment
+env | grep GHCUP_ >> /etc/agent_environment
+env | grep NVM_ >> /etc/agent_environment
+env | grep SELENIUM_ >> /etc/agent_environment
+env | grep SWIFT_ >> /etc/agent_environment
+env | grep VCPKG_ >> /etc/agent_environment
+env | grep DOTNET_ >> /etc/agent_environment
+env | grep LANG >> /etc/agent_environment
+env | grep M2_ >> /etc/agent_environment
+env | grep VSTS_ >> /etc/agent_environment
+env | grep LD_ >> /etc/agent_environment
+env | grep PERL5LIB >> /etc/agent_environment
