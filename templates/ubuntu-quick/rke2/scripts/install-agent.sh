c3383507183436747dbc5f0ca47f0f399e273c64
Matt Gerega <gerega@gmail.com>
2024-02-08 09:01:54 -0500

    Set RKE2 version on agent

--- templates/ubuntu-quick/rke2/scripts/install-agent.sh
+++ templates/ubuntu-quick/rke2/scripts/install-agent.sh
@@ -2,7 +2,9 @@
 curl -sfL https://get.rke2.io --output install.sh
 sudo chmod +x install.sh
 
-sudo INSTALL_RKE2_TYPE="agent" ./install.sh
+sudo export INSTALL_RKE2_VERSION="v1.26.12+rke2r1"
+sudo export INSTALL_RKE2_TYPE="agent"
+sudo ./install.sh
 
 sudo systemctl enable rke2-agent.service
 sudo mkdir -p /etc/rancher/rke2
