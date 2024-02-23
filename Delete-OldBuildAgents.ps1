52d7b8b4311b67788e8aa2a6d34056e5ef9adc28
Matt Gerega <gerega@gmail.com>
2024-02-23 10:25:33 -0500

    Updated deprovisioning script

--- Delete-OldBuildAgents.ps1
+++ Delete-OldBuildAgents.ps1
@@ -25,6 +25,8 @@
 #>
 
 param (
+    [string] $devOpsOrg,    
+    [string] $devOpsUsername,
     [string] $devOpsPat,
     [string] $devOpsPool,
     [string] $agentUser,
@@ -34,16 +36,39 @@ param (
 
 Import-Module ./HyperV-Provisioning.psm1
 
+# Turn the string into a base64 encoded string
+$bytes = [System.Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f ($devOpsUsername, $devOpsPat)))
+$token = [System.Convert]::ToBase64String($bytes)
+# Define a basic 'Authorization' header with the token
+$headers = @{
+    Authorization = "Basic {0}" -f ($token)
+}
 
-$vms = Get-Vm agt-ubt-*
+$poolSearch = Invoke-RestMethod -Uri "https://dev.azure.com/$($devOpsOrg)/_apis/distributedtask/pools?poolName=$($devOpsPool)" -Headers $headers
+
+if ($poolSearch.count -eq 0) {
+    Write-Host "Pool $devOpsPool not found"
+    exit
+}
+
+$poolId = $poolSearch.value.id
+
+$agents = Invoke-RestMethod -Uri "https://dev.azure.com/$($devOpsOrg)/_apis/distributedtask/pools/$($poolId)/agents" -Headers $headers
+
+$agentNames = $agents | Select-Object -Property name
+
+$vms = Get-Vm agt-ubt-* | Where-Object { $_.Name -in $agentNames.name }
 
 if ($vms.Count -gt 1) {
     Write-Host "Removing old agents"
 
-    $vms | Select-Object -Property Name, @{ Name="Date"; Expression={[DateTime]::ParseExact($_.Name.Replace("agt-ubt-", ""), 'yyMMdd', $null)}} 
+    $vms  | Select-Object -Property Name, @{ Name="Date"; Expression={[DateTime]::ParseExact($_.Name.Replace("agt-ubt-", ""), 'yyMMdd', $null)}} 
         | sort-object -property date | Select-Object -First ($vms.Count - 1) 
         | ForEach-Object {
             Write-Host "Removing $($_.Name)"
             ##Remove-HyperVVm -machinename $($_.Name) -isMsAgent $true -msAgentPAT $devOpsPat -userName $agentUser -password $agentPassword
         }
+}
+else {
+    Write-Host "No old agents to remove"
 }
