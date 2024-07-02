26cf21338e3edef5f571b674afceade21ed99c35
Matt Gerega <gerega@gmail.com>
2024-07-02 14:43:12 -0400

    Fix hostname in provisioning

--- templates/ubuntu-quick/basic/basic-prov.sh
+++ templates/ubuntu-quick/basic/basic-prov.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 # change hostname
 
-$RELEASE_VER=$(lsb_release -r)
-$NUM_VER=$(cut -f2 <<< "$RELEASE_VER")
+RELEASE_VER=$(lsb_release -r)
+NUM_VER=$(cut -f2 <<< "$RELEASE_VER")
 
-if [ $NUM_VER -eg "24.04"]
+if [ $NUM_VER -eq "24.04"]
 then
     sudo hostnamectl set-hostname "$VM_NAME"
     sudo sed -i "s#$BASE_NAME#$VM_NAME#g" /etc/hostname
