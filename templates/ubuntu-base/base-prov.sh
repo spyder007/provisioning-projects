c11c7535fba56fe547b43b727bb906b1a84882a0
Matt Gerega <gerega@gmail.com>
2024-10-02 07:48:41 -0400

    re-add nfs-common to provisioning

--- templates/ubuntu-base/base-prov.sh
+++ templates/ubuntu-base/base-prov.sh
@@ -3,5 +3,6 @@
 sudo lvextend -l+100%FREE $(sudo lvdisplay -C -o "lv_path" --noheadings)
 sudo resize2fs $(df / --output=source | sed -e /Filesystem/d)
 sudo apt update
+sudo apt install -y nfs-common
 sudo apt upgrade -y
 sudo apt autoremove -y
