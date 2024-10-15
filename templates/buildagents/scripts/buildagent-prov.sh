b9f2f21c06fe10f94b6f49f44961b4f5cfe4282a
Matt Gerega <gerega@gmail.com>
2024-10-15 10:56:36 -0400

    Moved linux-cloud-tools-virtual provisioning

--- templates/buildagents/scripts/buildagent-prov.sh
+++ templates/buildagents/scripts/buildagent-prov.sh
@@ -4,6 +4,7 @@ sudo lvextend -l+100%FREE $(sudo lvdisplay -C -o "lv_path" --noheadings)
 sudo resize2fs $(df / --output=source | sed -e /Filesystem/d)
 sudo apt update
 sudo apt upgrade -y
+sudo apt install -y linux-cloud-tools-virtual
 mkdir ~/.ssh
 cp /imagegeneration/authorized_keys ~/.ssh/
 
--- templates/ubuntu-base/base-prov.sh
+++ templates/ubuntu-base/base-prov.sh
@@ -3,6 +3,6 @@
 sudo lvextend -l+100%FREE $(sudo lvdisplay -C -o "lv_path" --noheadings)
 sudo resize2fs $(df / --output=source | sed -e /Filesystem/d)
 sudo apt update
-sudo apt install -y nfs-common
+sudo apt install -y nfs-common linux-cloud-tools-virtual
 sudo apt upgrade -y
 sudo apt autoremove -y
