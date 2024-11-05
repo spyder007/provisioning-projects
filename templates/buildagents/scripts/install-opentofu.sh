e979ba4b61819d2f907b0a73f7c77a23b8561018
Matt Gerega <gerega@gmail.com>
2024-11-05 14:39:22 -0500

    Update runner images and add opentofu install

---
+++ templates/buildagents/scripts/install-opentofu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+################################################################################
+##  File:  install-opentofu.sh
+##  Desc:  Install opentofu
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/etc-environment.sh
+
+# Install Miniconda
+curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh \
+    && chmod +x install-opentofu.sh \
+    && ./install-opentofu.sh --install-method deb \
+    && rm install-opentofu.sh
+
+OPENTOFU=/usr/share/tofu
+set_etc_environment_variable "OPENTOFU" "${OPENTOFU}"
+
+ln -s $OPENTOFU/bin/tofu /usr/bin/tofu
