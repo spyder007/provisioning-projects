078ff41a4fd046c76414825ef492993bb9db56e6
Matt Gerega <gerega@gmail.com>
2024-11-19 17:43:56 -0500

    Add default https folder to HOME directory

--- templates/buildagents/scripts/configure-buildagent.sh
+++ templates/buildagents/scripts/configure-buildagent.sh
@@ -41,4 +41,5 @@ sudo chmod -R 777 /myagent
 ./config.sh --unattended  --url "$MS_AGENT_ORG_URL" --auth pat --token "$MS_AGENT_PAT" --pool "$MS_AGENT_POOL_NAME"
 sudo ./svc.sh install
 sudo ./svc.sh start
+mkdir -p ~/.aspnet/https
 exit 0
